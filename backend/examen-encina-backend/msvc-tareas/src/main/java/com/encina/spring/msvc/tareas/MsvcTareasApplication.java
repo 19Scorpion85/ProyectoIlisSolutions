b58c921f2c6178fb18953488b212f76a166c26d9
19Scorpion85 <81202367+19Scorpion85@users.noreply.github.com>
2023-07-25 09:55:37 -0400

    add msvc-tareas

---
+++ backend/examen-encina-backend/msvc-tareas/src/main/java/com/encina/spring/msvc/tareas/MsvcTareasApplication.java
@@ -0,0 +1,13 @@
+package com.encina.spring.msvc.tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcTareasApplication.class, args);
+	}
+
+}
---
+++ backend/examen-encina-backend/msvc-tareas/src/test/java/com/encina/spring/msvc/tareas/MsvcTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encina.spring.msvc.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
