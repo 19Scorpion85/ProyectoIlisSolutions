40346aea1635182691b59cddd7013a6091a4ba53
19Scorpion85 <81202367+19Scorpion85@users.noreply.github.com>
2023-07-25 10:32:15 -0400

    Create clase TareaServiceImpl
    En package service

---
+++ backend/examen-encina-backend/msvc-tareas/src/main/java/com/encina/spring/examen/msvc/tarea/service/TareaServiceImpl.java
@@ -0,0 +1,40 @@
+package com.encina.spring.examen.msvc.tarea.service;
+
+import com.encina.spring.examen.msvc.tarea.models.entities.Tarea;
+import com.encina.spring.examen.msvc.tarea.repositories.TareaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class TareaServiceImpl implements  TareaService{
+
+    @Autowired
+    private TareaRepository repository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Tarea> Listar() {
+        return (List<Tarea>) repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Tarea> porId(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Tarea guardar(Tarea tarea) {
+        return repository.save(tarea);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+}
