5eea2b7259594379d91f55b8bc4752334b6e126c
19Scorpion85 <81202367+19Scorpion85@users.noreply.github.com>
2023-07-25 10:19:20 -0400

    Create interface TareaRepository
    Dentro de package repositories

---
+++ backend/examen-encina-backend/msvc-tareas/src/main/java/com/encina/spring/examen/msvc/tarea/repositories/TareaRepository.java
@@ -0,0 +1,8 @@
+package com.encina.spring.examen.msvc.tarea.repositories;
+
+import com.encina.spring.examen.msvc.tarea.models.entities.Tarea;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TareaRepository extends CrudRepository<Tarea, Long> {
+
+}
