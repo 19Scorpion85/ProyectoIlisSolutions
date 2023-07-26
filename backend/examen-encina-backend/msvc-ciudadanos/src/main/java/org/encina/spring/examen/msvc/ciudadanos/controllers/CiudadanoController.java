4eaacbc697674d0bf7081d6c1a7ac2fc2a0b5f02
19Scorpion85 <81202367+19Scorpion85@users.noreply.github.com>
2023-07-26 10:05:19 -0400

    Create crossOrigin

--- backend/examen-encina-backend/msvc-ciudadanos/src/main/java/org/encina/spring/examen/msvc/ciudadanos/controllers/CiudadanoController.java
+++ backend/examen-encina-backend/msvc-ciudadanos/src/main/java/org/encina/spring/examen/msvc/ciudadanos/controllers/CiudadanoController.java
@@ -14,6 +14,7 @@ import java.util.*;
  * @version: 24/07/2023/A
  * @See: "org.encina.spring.examen.msvc.usuarios/services/CiudadanoService"
  */
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class CiudadanoController {
     //Inicio controladores
