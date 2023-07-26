049c0b7ecddd34a2abf6219ff91671bc894af73c
19Scorpion85 <81202367+19Scorpion85@users.noreply.github.com>
2023-07-25 23:46:59 -0400

    Creación de componentes
    clientes
    footer
    header

--- frontend/ciudadanos-app/src/app/ciudadanos/ciudadanos.component.spec.ts
+++ frontend/ciudadanos-app/src/app/ciudadanos/ciudadanos.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { CiudadanosComponent } from './ciudadanos.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('CiudadanosComponent', () => {
+  let component: CiudadanosComponent;
+  let fixture: ComponentFixture<CiudadanosComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ CiudadanosComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(CiudadanosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- frontend/ciudadanos-app/src/app/header/header.component.ts
+++ frontend/ciudadanos-app/src/app/header/header.component.ts
@@ -2,14 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeaderComponent {
+  title: string = 'App ciudadanos de Eastview'
 }
