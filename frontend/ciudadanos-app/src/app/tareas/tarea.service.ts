57087403587a65f0113735a4404c805f0fc45d0a
19Scorpion85 <81202367+19Scorpion85@users.noreply.github.com>
2023-07-26 10:11:47 -0400

    Add crossOrigin en tarea

--- frontend/ciudadanos-app/src/app/tareas/tarea.service.ts
+++ frontend/ciudadanos-app/src/app/tareas/tarea.service.ts
@@ -4,16 +4,22 @@ import {Router} from '@angular/router';
 import { TAREAS } from './tarea.json';
 import { Tarea } from './tarea';
 import {HttpClient,HttpHeaders,HttpRequest,HttpEvent} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
 
+  private urlEndPoint:string='http://localhost:8002/';
+
   constructor(private http:HttpClient,private router:Router) { }
 
   getTareas():Observable<Tarea[]>{
-    return  of(TAREAS);
+   // return  of(TAREAS);
+   return this.http.get<Tarea[]>(this.urlEndPoint).pipe(
+    map((response)=>response as Tarea[])
+  );
   }
 
 }
